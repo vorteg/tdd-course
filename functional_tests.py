9e7860f0bd30bbc97acba0202339db60898b575b
victor ortega <vorteg.r@gmail.com>
2023-03-14 13:33:19 -0600

    Using unittest to provide a format logs and better workflow to tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "The install worked successfully! Congratulations!" in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edit has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get("http://localhost:8000")
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
+
+        # She is invited to enter a to-do item straight away
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
