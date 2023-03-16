de5571662ce1f8a25d7ed7019a1ed122e3a9e73d
victor ortega <vorteg.r@gmail.com>
2023-03-16 10:18:17 -0600

    Updating  selenium sintax by new version changes

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 import time
 import unittest
 from selenium import webdriver
+#There is a change to use `*find_element` now is using `by` methode
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
@@ -18,12 +20,12 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
         
         
         #She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -31,15 +33,15 @@ class NewVisitorTest(unittest.TestCase):
         
         #She types "buy peacock feathers" into a text box (Edith's hobby 
         # is tying fly-fishing lures)
-        input.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
         
         # WHen she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows)
         )
