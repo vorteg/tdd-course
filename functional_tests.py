0e0c8b0f4a1943430c1ab47bc74883abf185e3d9
victor ortega <vorteg.r@gmail.com>
2023-03-22 12:05:34 -0600

    FT to evaluate to list items with an helper fn

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self) -> None:
         self.browser.quit()
+        
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edit has heard about a cool new online to-do app. She goes
@@ -39,13 +44,25 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertTrue(
-            any(row.text == 'Buy peacock feathers' for row in rows), 
-            f"New to-do item did not apper in table. Contents were: \n{table.text}"
-        )
+        #Ther is stil a text  box inviting her to add another item. SHe
+        # enters "Use peacock feathers to make a fly" ( Edith is very 
+        # methodical)
+        inputbox = self. browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Buy peacock feathers to make a fly')
+        
+        # table = self.browser.find_element(By.ID, 'id_list_table')
+        # rows = table.find_elements(By.TAG_NAME,'tr')
+        # self.assertTrue(
+        #     any(row.text == 'Buy peacock feathers' for row in rows), 
+        #     f"New to-do item did not apper in table. Contents were: \n{table.text}"
+        # )
         
         # There is still a text box inviting her to add another item. She 
         # enters "Use peacock feathers to make a fly" (Edith is very
